945ec68fea9969a5ed448a8ca9f249e403a4bef9
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-09-15 09:58:26 -0600

    componentes base de atrjeta de zonas

---
+++ src/componentes/TarjetaEspacio.jsx
@@ -0,0 +1,16 @@
+export function TarjetaEspacio(){
+    return(
+
+        <>
+            <section className="card p-4 shadow h-100">
+                <img src="" alt="" className="img-fluid"/>
+                <h5 className="card-title fw-bold"></h5>
+                <p className="text-muted"></p>
+                <h3></h3>
+
+            </section>
+
+        </>
+
+    )
+}
---
+++ src/services/datosEspacios.js
@@ -0,0 +1,38 @@
+export const espacios=[
+    {
+        id:1,
+        nombre:"Piscina",
+        descripcion:"Zona humeda con piscina para niños y adultos",
+        foto:"https://images.unsplash.com/photo-1591285713698-598d587de63e?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        aforo:40
+    },
+    {
+        id:2,
+        nombre:"Salon Social",
+        descripcion:"Espacio para celebraciones y reuniones",
+        foto:"https://images.unsplash.com/photo-1621293954908-907159247fc8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        aforo:80
+    },
+    {
+        id:3,
+        nombre:"Gimnasio",
+        descripcion:"Equipos de cardio y fuerza para tu salud",
+        foto:"https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        aforo:10
+    },
+    {
+        id:4,
+        nombre:"Zona BBQ",
+        descripcion:"Asadores y mesas al aire libre para tus encuentros",
+        foto:"https://plus.unsplash.com/premium_photo-1693221705288-7a2531eaa5e5?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        aforo:60
+    },
+    {
+        id:5,
+        nombre:"Cancha Multiple",
+        descripcion:"Cancha para microfutbol y baloncesto",
+        foto:"https://plus.unsplash.com/premium_photo-1724155541930-7cde0213dcc9?q=80&w=1148&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        aforo:25
+    }
+    
+]
